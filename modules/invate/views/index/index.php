eb08fb3599cb9a03971ba71d364e098f8574a70c
chenlong <1004076836@qq.com>
2016-07-28 10:49:12 +0800

    第三次上传

--- modules/invate/views/index/index.php
+++ modules/invate/views/index/index.php
@@ -17,9 +17,10 @@
     </div>
         <div class="searchBar">
             <ul class="searchContent" style="margin-top: 10px;">
-                
+
                 <li>
                     <span>新增邀请码数量:</span>
+                    
                 </li>
                 <li>
                     <span>剩余邀请码数量:</span>
