563978e3a2a1fbaec1e0c251cbc8e9f9a286fd4b
chenlong <1004076836@qq.com>
2016-08-15 15:56:23 +0800

    修改bug

--- modules/audit/controllers/ReduceController.php
+++ modules/audit/controllers/ReduceController.php
@@ -49,7 +49,7 @@ class ReduceController extends CommonController
         $reduceModel= new ReduceModel();
         $post=Yii::$app->request->post();
         $sql=self::sql();
-        $sql=$sql." where a.`audit_time` is  not null and a.`audit_name` is not null";
+        $sql=$sql." where a.`audit_time` is  not null and a.`audit_name` is  not null";
         if($post){
             if($post["user_name"]){
                 $sql=$sql." and  b.`e_user_name`='{$post["user_name"]}'";
@@ -121,13 +121,13 @@ class ReduceController extends CommonController
             "trade_no"=>$data["trade_no"]
         ];
         $curl= new Curl();
-       $reduce_result_json=$curl->setOption(CURLOPT_POSTFIELDS, http_build_query($message))->post(yii::$app->params['reduce_url']);
+        $reduce_result_json=$curl->setOption(CURLOPT_POSTFIELDS, http_build_query($message))->post(yii::$app->params['reduce_url']);
         $reduce_result=json_decode($reduce_result_json,true);
         if($reduce_result["code"]==200){
           $reduceModel->updateAll(["flag"=>3004,"audit_time"=>time(),"audit_name"=>Tools::get_user_name()],["id"=>$id]);
           return Functions::return_json(200,"提现成功","","reduce_id_index","closeCurrent");
       }
-        $reduceModel->updateAll(["flag"=>3005],["id"=>$id]);
+        $reduceModel->updateAll(["flag"=>3004],["id"=>$id]);
         return Functions::return_json(300,$reduce_result["data"]["return_msg"],"","reduce_id_index","closeCurrent");
     }
 
