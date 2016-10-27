9ef3ad3161543cbfc981e9ac288822a5de1d3b70
chenchenlonglong <1004076836@qq.com>
2016-10-27 09:42:55 +0800

    麦田社货号

--- config/db.php
+++ config/db.php
@@ -1,17 +1,17 @@
 <?php
 
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=120.26.223.3;dbname=maitianpin',
-//    'username' => 'caotianyin',
-//    'password' => 'caotianyin!@#',
-//    'charset' => 'utf8',
-//];
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=112.126.90.51;dbname=maitianpin',
-    'username' => 'root',
-    'password' => 'JUSTDOloveyou',
+    'dsn' => 'mysql:host=120.26.223.3;dbname=maitianpin',
+    'username' => 'caotianyin',
+    'password' => 'caotianyin!@#',
     'charset' => 'utf8',
 ];
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=112.126.90.51;dbname=maitianpin',
+//    'username' => 'root',
+//    'password' => 'JUSTDOloveyou',
+//    'charset' => 'utf8',
+//];
